da5f117ef143f4e48f943fd8393633f78ef8068c
Sriram <72183029+SriramSonaganti@users.noreply.github.com>
2021-09-09 22:33:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,20 +14,35 @@ app = Flask(__name__)
 def hello():
     return 'hello'
 
-@app.route('/predict-image', methods=['POST','GET'])
-def detect():
-    # img=request.args['image']
-    # img_path='images/'+img
-    # results=detectWifiRouter(img_path)
-    # return jsonify(results)
-    if (request.method == 'POST'):
-        if request.files:
-            image = Image.open(request.files['image'])
-            numpydata = asarray(image)
-            result = detectWifiRouter(numpydata)
-            return result
 
-        return "file not saved"
+@app.route('/predict-audio',methods=['POST','GET'])
+def prediction():
+    if request.method=='GET':
+        if len(ans)!=0:
+            return str(ans[-1])
+        else:
+            return "send your audio format"
+    if request.method=='POST':
+        if request.files:
+            f=request.files.get('file')
+            x, sr = librosa.load(f, res_type='kaiser_fast')
+            mfccs = np.mean(librosa.feature.mfcc(y=x, sr=sr, n_mfcc=40).T, axis=0)
+            mfccs = mfccs.reshape(1, -1)
+            # print(mfccs)
+            predict_prob = model.predict(mfccs)
+            # print(predict_prob)
+            predicted_label = np.argmax(predict_prob, axis=1)
+            # print(predicted_label)
+            for i in predicted_label:
+                print(classes[i])
+                ans.append(classes[i])
+            return str(ans[-1])
+    return "files not saved"
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run()
